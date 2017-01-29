b0ad7086689a97b307afbffa62dc00905fedd40b
alex920a <alex920a@gmail.com>
2017-01-29 12:04:58 +0100

    Added main menu + MovingController

---
+++ CrashClone/Assets/Scripts/CameraFollowing.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowing : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject crashGameObject;
+    private Vector3 offsetPosition;
+    //private Quaternion offsetRotation;
+
+	// Use this for initialization
+	void Start () {
+
+        offsetPosition = transform.position - crashGameObject.transform.position;
+        print("Offset Position:" + offsetPosition);
+        //offsetRotation = transform.rotation 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        transform.position = crashGameObject.transform.position + offsetPosition;
+        print("Camera position: " + transform.position);
+	}
+}
---
+++ CrashClone/Assets/Scripts/MovingController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingController : MonoBehaviour {
+
+    //controller for crash movements
+
+    private float speed = 10f;
+    private float jumpSpeed = 5f;
+    private float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+
+    private CharacterController movementsController;
+
+    void Start()
+    {
+        movementsController = gameObject.GetComponent<CharacterController>();
+
+        if(movementsController == null)
+        {
+            Debug.LogError("Cannot find CharacterController!");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if (Input.GetKey(KeyCode.W))
+        //    gameObject.transform.position += Vector3.forward * speed * Time.deltaTime;
+        if(movementsController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+            //moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                moveDirection.y = jumpSpeed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        movementsController.Move(moveDirection * Time.deltaTime);
+        
+	}
+}
